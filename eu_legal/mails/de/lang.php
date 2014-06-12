6ca73aee5a56e948bbbc147724a0f33cf8df3650
Chris Gurk <t_github@gurkcity.de>
2014-06-12 15:15:42 +0200

    new theme mails

---
+++ eu_legal/mails/de/lang.php
@@ -0,0 +1,44 @@
+<?php
+
+global $_LANGMAIL;
+$_LANGMAIL = array();
+
+$_LANGMAIL['Your guest account has been transformed to customer account'] = 'Ihr Gast-Konto wurde in ein Kunden-Konto umgewandelt';
+$_LANGMAIL['Virtual product to download'] = 'Virtuelle(r) Artikel zum Download';
+$_LANGMAIL['New voucher regarding your order %s'] = 'Neue Gutschein für Ihre Bestellung %s';
+$_LANGMAIL['Order confirmation'] = 'Auftragsbestätigung';
+$_LANGMAIL['Log: You have a new alert from your shop'] = 'Sie haben eine neue Log-Warnung von Ihrem Shop erhalten';
+$_LANGMAIL['Fwd: Customer message'] = 'Fwd: Kundennachricht';
+$_LANGMAIL['An answer to your message is available #ct%1$s #tc%2$s'] = 'Sie haben eine Antwort auf Ihre Nachricht erhalten #ct%1$s #tc%2$s';
+$_LANGMAIL['Your new password'] = 'Ihr neues Kennwort';
+$_LANGMAIL['Package in transit'] = 'Paket versendet';
+$_LANGMAIL['New message regarding your order'] = 'Neue Nachricht zu Ihrer Bestellung';
+$_LANGMAIL['New credit slip regarding your order'] = 'Rückvergütung für Ihre Bestellung';
+$_LANGMAIL['Process the payment of your order'] = 'Bezahlung Ihrer Bestellung';
+$_LANGMAIL['Your order has been changed'] = 'Ihre Bestellung wurde geändert';
+$_LANGMAIL['Your order return state has changed'] = 'Der Status Ihrer Rücksendung wurde geändert';
+$_LANGMAIL['Welcome!'] = 'Willkommen';
+$_LANGMAIL['Your message has been correctly sent #ct%1$s #tc%2$s'] = 'Ihre Nachricht wurde gesendet #ct%1$s #tc%2$s';
+$_LANGMAIL['Your message has been correctly sent'] = 'Ihre Nachricht wurde gesendet';
+$_LANGMAIL['Message from contact form'] = 'Nachricht aus dem Kontaktformular';
+$_LANGMAIL['Message from a customer'] = 'Nachricht eines Kunden';
+$_LANGMAIL['Password query confirmation'] = 'Passwort vergessen';
+$_LANGMAIL['Newsletter voucher'] = 'Newsletter-Gutschein';
+$_LANGMAIL['Newsletter confirmation'] = 'Newsletterabo-Bestätigung';
+$_LANGMAIL['Email verification'] = 'Bestätigungsmail';
+$_LANGMAIL['Your wishlist\'s link'] = 'Link zu Ihrer Wunschliste';
+$_LANGMAIL['Message from %1$s %2$s'] = 'Nachricht von %1$s %2$s';
+$_LANGMAIL['Your cart and your discount'] = 'Rabattangebot für Artikel in Ihrem Warenkorb';
+$_LANGMAIL['Thanks for your order'] = 'Vielen Dank für Ihre Bestellung!';
+$_LANGMAIL['You are one of our best customers'] = 'Als Daneschön dafür, dass Sie zu unseren besten Kunden gehören';
+$_LANGMAIL['We miss you'] = 'Wir vermissen Sie';
+$_LANGMAIL['Product available'] = 'Der Artikel auf Ihrer Beobachtungsliste ist wieder lieferbar';
+$_LANGMAIL['New order : #%d - %s'] = 'Neue Bestellung: Nr. %d - %s';
+$_LANGMAIL['Product out of stock'] = 'Ein oder mehrere Artikel sind fast ausverkauft';
+$_LANGMAIL['Stock coverage'] = 'Bestandsdeckung';
+$_LANGMAIL['Referral Program'] = 'Mein Konto';
+$_LANGMAIL['Congratulations!'] = 'Gratulation';
+$_LANGMAIL['%1$s sent you a link to %2$s'] = '%1$s hat Ihnen einen Link von %2$s geschickt';
+
+
+return $_LANGMAIL;
