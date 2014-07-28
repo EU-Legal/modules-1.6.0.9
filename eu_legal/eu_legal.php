85b96e614370dd5dc97733d30bbe4b5d2caf3ab7
Chris Gurk <t_github@gurkcity.de>
2014-07-28 20:07:43 +0200

    Bugfix: shipping link in shopping cart in SSL environment

--- eu_legal/eu_legal.php
+++ eu_legal/eu_legal.php
@@ -4,8 +4,8 @@
 * EU Legal
 * Better security for German and EU merchants.
 * 
-* @version       : 1.0.0
-* @date          : 2014 07 04
+* @version       : 1.0.1
+* @date          : 2014 07 28
 * @author        : Markus Engel/Chris Gurk @ Onlineshop-Module.de | George June/Alexey Dermenzhy @ Silbersaiten.de
 * @copyright     : 2014 Onlineshop-Module.de | 2014 Silbersaiten.de
 * @contact       : info@onlineshop-module.de | info@silbersaiten.de
@@ -59,7 +59,7 @@ class EU_Legal extends Module {
 		$this->tab = 'administration';       
 	 	
 		// version: major, minor, bugfix
-		$this->version = '1.0.0';                
+		$this->version = '1.0.1';                
 		
 		// author
 		$this->author = 'EU Legal Team'; 
@@ -1982,7 +1982,7 @@ class EU_Legal extends Module {
 		$cms = new CMS(Configuration::get('LEGAL_CMS_ID_SHIPPING'));
 		
 		if (Validate::isLoadedObject($cms)) {
-		    $shipping_link = $this->context->link->getCMSLink($cms);
+		    $shipping_link = $this->context->link->getCMSLink($cms, null, Configuration::get('PS_SSL_ENABLED'));
 
 		    if ( ! strpos($shipping_link, '?')) {
 			$shipping_link.= '?content_only=1';
