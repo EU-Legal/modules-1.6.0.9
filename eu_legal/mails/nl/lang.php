326887fa381aba3d997dcbf750831bc7bfb0616d
mBangiev <m.bangiev@silbersaiten.de>
2014-10-01 13:47:32 +0200

    added eu mail tempaltes for other languages

---
+++ eu_legal/mails/nl/lang.php
@@ -0,0 +1,45 @@
+<?php
+
+global $_LANGMAIL;
+$_LANGMAIL = array();
+
+$_LANGMAIL['Your guest account has been transformed into a customer account'] = 'Uw gastaccount is omgezet in een klantaccount';
+$_LANGMAIL['The virtual product that you bought is available for download'] = 'Het virtuele product dat u heeft aangekocht is beschikbaar om te downloaden';
+$_LANGMAIL['New voucher for your order %s'] = 'Nieuwe waardebon betreffende uw bestelling %s';
+$_LANGMAIL['Order confirmation'] = 'Bestelling bevestigen';
+$_LANGMAIL['Log: You have a new alert from your shop'] = 'Log: u heeft een nieuwe waarschuwing in uw winkel ontvangen';
+$_LANGMAIL['Fwd: Customer message'] = 'Doorsturen: klantenbericht';
+$_LANGMAIL['An answer to your message is available #ct%1$s #tc%2$s'] = 'Een antwoord op uw bericht is beschikbaar  #ct%1$s #tc%2$s';
+$_LANGMAIL['Your new password'] = 'Uw nieuwe wachtwoord';
+$_LANGMAIL['Package in transit'] = 'Pakket is verzonden';
+$_LANGMAIL['New message regarding your order'] = 'Nieuw bericht betreffende uw bestelling';
+$_LANGMAIL['New voucher for your order #%s'] = 'Nieuwe waardebon voor uw bestelling %s';
+$_LANGMAIL['New credit slip regarding your order'] = 'Nieuwe creditnota voor uw bestelling';
+$_LANGMAIL['Process the payment of your order'] = 'Verwerk de betaling van uw bestelling';
+$_LANGMAIL['Your order has been changed'] = 'Uw bestelling is gewijzigd';
+$_LANGMAIL['Your order return status has changed'] = 'Uw retourzending status is gewijzigd';
+$_LANGMAIL['Welcome!'] = 'Welkom!';
+$_LANGMAIL['Your message has been correctly sent #ct%1$s #tc%2$s'] = 'Uw bericht is correct verzonden #ct%1$s #tc%2$s';
+$_LANGMAIL['Your message has been correctly sent'] = 'Uw bericht is correct verzonden';
+$_LANGMAIL['Message from contact form'] = 'Bericht via het contactformulier';
+$_LANGMAIL['Message from a customer'] = 'Bericht van een klant';
+$_LANGMAIL['Password query confirmation'] = 'Wachtwoord aanvraag bevestiging';
+$_LANGMAIL['Newsletter voucher'] = 'Nieuwsbrief waardebon';
+$_LANGMAIL['Newsletter confirmation'] = 'Bevestiging nieuwsbrief';
+$_LANGMAIL['Email verification'] = 'E-mail verificatie';
+$_LANGMAIL['Your wishlist\'s link'] = 'Uw verlanglijst link';
+$_LANGMAIL['Message from %1$s %2$s'] = 'Bericht van %1$s %2$s';
+$_LANGMAIL['Your cart and your discount'] = 'Uw winkelwagen en uw korting';
+$_LANGMAIL['Thanks for your order'] = 'Dank u wel voor uw bestelling';
+$_LANGMAIL['You are one of our best customers'] = 'U bent één van onze beste klanten';
+$_LANGMAIL['We miss you'] = 'Wij missen u';
+$_LANGMAIL['Product available'] = 'Product beschikbaar';
+$_LANGMAIL['New order : #%d - %s'] = 'Nieuwe bestelling : #%d - %s';
+$_LANGMAIL['Product out of stock'] = 'Product uit voorraad';
+$_LANGMAIL['Stock coverage'] = 'Voorraaddekking';
+$_LANGMAIL['Referral Program'] = 'Referentie programma';
+$_LANGMAIL['Congratulations!'] = 'Gefeliciteerd!';
+$_LANGMAIL['%1$s sent you a link to %2$s'] = '%1$s stuurt u een link naar %2$s';
+
+
+return $_LANGMAIL;
---
+++ eu_legal/mails/pl/lang.php
@@ -0,0 +1,41 @@
+<?php
+
+global $_LANGMAIL;
+$_LANGMAIL = array();
+
+$_LANGMAIL['Order confirmation'] = 'Potwierdzenie zamówienia';
+$_LANGMAIL['Log: You have a new alert from your shop'] = 'Log: masz nowe ostrzeżenie z Twojego sklepu';
+$_LANGMAIL['Fwd: Customer message'] = 'TR: Wiadomość od klienta';
+$_LANGMAIL['An answer to your message is available #ct%1$s #tc%2$s'] = 'Dostępna jest odpowiedź na Twoją wiadomość #ct%1$s #tc%2$s';
+$_LANGMAIL['Your new password'] = 'Twoje nowe hasło';
+$_LANGMAIL['Package in transit'] = 'Dostawa w trakcie realizacji';
+$_LANGMAIL['New message regarding your order'] = 'Nowa wiadomość dotycząca zamówienia';
+$_LANGMAIL['New credit slip regarding your order'] = 'Nowy \'kredyt\' dotyczący zamówienia';
+$_LANGMAIL['Process the payment of your order'] = 'Przetwórz płatność dla Twojego zamówienia';
+$_LANGMAIL['Your order has been changed'] = 'Twoje zamówienie zostało zmodyfikowane';
+$_LANGMAIL['Your order return status has changed'] = 'Nowy status zamówienia';
+$_LANGMAIL['Welcome!'] = 'Witaj !';
+$_LANGMAIL['Your message has been correctly sent #ct%1$s #tc%2$s'] = 'Twoja wiadomość została prawidłowo wysłana #ct%1$s #tc%2$s';
+$_LANGMAIL['Your message has been correctly sent'] = 'Twoja wiadomość została prawidłowo wysłana';
+$_LANGMAIL['Message from contact form'] = 'Wiadomość  od';
+$_LANGMAIL['Message from a customer'] = 'Wiadomość od klienta';
+$_LANGMAIL['Password query confirmation'] = 'Potwierdzenia prośby o hasło';
+$_LANGMAIL['Newsletter voucher'] = 'Voucher za newsletter';
+$_LANGMAIL['Newsletter confirmation'] = 'Potwierdzenie newslettera';
+$_LANGMAIL['Email verification'] = 'E-mail weryfikacyjny';
+$_LANGMAIL['Your wishlist\'s link'] = 'Twój link listy życzeń';
+$_LANGMAIL['Message from %1$s %2$s'] = 'Wiadomość od  %1$s %2$s';
+$_LANGMAIL['Your cart and your discount'] = 'Twój koszyk i Twój kupon rabatowy';
+$_LANGMAIL['Thanks for your order'] = 'Dziękujemy za dokonanie zamówienia';
+$_LANGMAIL['You are one of our best customers'] = 'Jesteś jednym z naszych najlepszych klientów';
+$_LANGMAIL['We miss you'] = 'Tęsknimy za tobą';
+$_LANGMAIL['Product available'] = 'Dostępny produkt';
+$_LANGMAIL['New order : #%d - %s'] = 'Nowe zamówienie: #%d - %s';
+$_LANGMAIL['Product out of stock'] = 'Wyczerpanie zapasów';
+$_LANGMAIL['Stock coverage'] = 'Aktualne pokrycie stanu';
+$_LANGMAIL['Referral Program'] = 'Program sponsorowania (Referral)';
+$_LANGMAIL['Congratulations!'] = 'Gratulacje!';
+$_LANGMAIL['%1$s sent you a link to %2$s'] = '%1$s wysłano Ci link do %2$s';
+
+
+return $_LANGMAIL;
